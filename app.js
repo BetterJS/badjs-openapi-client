6d84665b1b807bef4f2f184c5ffe7355db9bc102
kael <yorts52@qq.com>
2016-03-14 18:58:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var net = require('net');
-var Events = require("events");
+var Events = require("events").EventEmitter;
 
 var TIME_OUT = 60 * 1000 * 10;
+
 /**
  * @param setting
  * @constructor
  */
-var OpenApiClient = function (options, callback) {
+function OpenApiClient(options, callback) {
     options || (options = {});
-    callback || (callback = function () {
-    });
+    callback || (callback = function() {});
 
     var self = this;
-
 
     if (!options.appkey) {
         throw "appkey is required ";
@@ -20,34 +19,41 @@ var OpenApiClient = function (options, callback) {
 
     this._events = new Events();
 
-    self._client = net.connect({port: options.port || 9500, host: options.host || "127.0.0.1"}, function (err) {
+    self._client = net.connect({
+        port: options.port || 9500,
+        host: options.host || "127.0.0.1"
+    }, function(err) {
         if (err) {
-            callback(err)
+            callback(err);
         }
-        self._client.write(JSON.stringify({type: "auth", appkey: options.appkey || ""}));
+        self._client.write(JSON.stringify({
+            type: "auth",
+            appkey: options.appkey || ""
+        }));
     });
 
+    this._timeoutId = setInterval(function() {
+        if (!self._closeflag) {
 
-    this._timeoutId = setInterval(function () {
-        if(!self._closeflag){
-
-            if(new Date - self._timeout > TIME_OUT){
+            if (new Date - self._timeout > TIME_OUT) {
                 self._events.emit("timeout");
                 self.close();
-                return ;
+                return;
             }
 
-            self._client.write(JSON.stringify({type: "keepalive"}))
+            self._client.write(JSON.stringify({
+                type: "keepalive"
+            }))
         }
     }, 1000 * 60 * 5);
 
-    this._timeout = new Date - 0
+    this._timeout = new Date - 0;
 
-
-    self._client.on("data", function (data) {
-        var json = {} , dataStr = "";
+    self._client.on("data", function(data) {
+        var json = {},
+            dataStr = "";
         try {
-            dataStr  = data.toString();
+            dataStr = data.toString();
             json = JSON.parse(dataStr);
         } catch (e) {
             callback(e);
@@ -55,7 +61,7 @@ var OpenApiClient = function (options, callback) {
         }
 
         switch (json.type) {
-            case "auth" :
+            case "auth":
                 if (json.err < 0) {
                     callback(json);
                 } else {
@@ -66,31 +72,30 @@ var OpenApiClient = function (options, callback) {
 
             case "keepalive":
                 self._timeout = new Date - 0;
-            default :
-                self._events.emit("data" , dataStr);
+            default:
+                self._events.emit("data", dataStr);
                 break;
         }
 
     });
 
-    self._client.on("close", function () {
+    self._client.on("close", function() {
         clearInterval(self._timeoutId);
         self._events.emit("close");
-    })
-
+    });
 }
 
 OpenApiClient.prototype = {
 
-    on: function () {
+    on: function() {
         this._events.addListener.apply(this._events, arguments);
     },
 
-    off: function () {
+    off: function() {
         this._events.removeListener.apply(this._events, arguments);
     },
 
-    close: function () {
+    close: function() {
         // destroy 异步触发close 时间， 存在可能 interval 先运行，造成 write 时候出错
         this._closeflag = true;
         this._client.destroy();
@@ -98,10 +103,6 @@ OpenApiClient.prototype = {
 
 }
 
-
-module.exports = function (options, callback) {
+module.exports = function(options, callback) {
     return new OpenApiClient(options, callback);
 }
-
-
-
